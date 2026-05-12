b7c9937ebddc79a8ba0b0bb1d6917833a40ffd51
Adarsh Shukla <adarshshukla09887@gmail.com>
2026-05-12 16:43:31 +0530

    inngest

---
+++ inngest/client.ts
@@ -0,0 +1,4 @@
+import { Inngest } from "inngest";
+
+// The ID is used to identify your app in the Inngest Cloud/Dev Server
+export const inngest = new Inngest({ id: "presentation-generator-ai" });
